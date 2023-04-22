9fff28ce51aae4a6e1e7cba586a71828835a4209
SAM-LCU <articulatemischief@gmail.com>
2023-04-22 19:54:38 +0530

    Ok

---
+++ functionInterface/LambdaExpr/CarEngine.java
@@ -0,0 +1,31 @@
+@FunctionalInterface
+//Method without passing arguments to function
+
+// interface Engine
+// {
+//    void eng();
+// }
+ 
+//Method with passing arguments to functions
+interface Engine
+ {
+   void eng(long cc);
+ }
+class CarEngine
+{
+    public static void main(String d[])
+    {
+        //We can define the method directly using Lambda Expression by reducing code size
+
+        // Engine obj = () -> System.out.println("A car has only one engine"); 
+        // obj.eng();
+         
+        
+        //Lambda Expressions for passing arguments
+        
+         Engine obj = cc -> System.out.println("It is a " + cc + " Engine" );
+         obj.eng(1498); 
+        
+
+    }
+}

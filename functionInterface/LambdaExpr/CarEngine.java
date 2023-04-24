e94044d7094baac61976410d6d05d4350336ad85
SAM-LCU <articulatemischief@gmail.com>
2023-04-24 20:41:14 +0530

    using Lambda Expr(Arrow Function)

--- functionInterface/LambdaExpr/CarEngine.java
+++ functionInterface/LambdaExpr/CarEngine.java
@@ -1,31 +1,29 @@
 @FunctionalInterface
 //Method without passing arguments to function
 
-// interface Engine
-// {
-//    void eng();
-// }
+interface Engine
+{
+   void eng();
+}
  
 //Method with passing arguments to functions
-interface Engine
- {
-   void eng(long cc);
- }
+// interface Engine
+//  {
+//    void eng(long cc);
+//  }
 class CarEngine
 {
     public static void main(String d[])
     {
         //We can define the method directly using Lambda Expression by reducing code size
 
-        // Engine obj = () -> System.out.println("A car has only one engine"); 
-        // obj.eng();
+        Engine obj = () -> System.out.println("A car has only one engine"); 
+        obj.eng();
          
         
         //Lambda Expressions for passing arguments
         
-         Engine obj = cc -> System.out.println("It is a " + cc + " Engine" );
-         obj.eng(1498); 
-        
-
+        //  Engine obj = cc -> System.out.println("It is a " + cc + " Engine" );
+        //  obj.eng(1498); 
     }
 }

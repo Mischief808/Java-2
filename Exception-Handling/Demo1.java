1646d4ae04d569c8190ddda3c23dc470dbb1eabe
SAM-LCU <articulatemischief@gmail.com>
2023-05-03 20:23:19 +0530

    Prog for user-defined Exception

--- Exception-Handling/Demo1.java
+++ Exception-Handling/Demo1.java
@@ -9,12 +9,12 @@ class Demo1
 {
    public static void main(String a[]) 
     {
-        int i=0;
+        int i=2;
         int j=0;
         try
         {
-            // j=18/i;
-            j=i/18;
+            j=18/i;
+            // j=i/18;
             if(j==0)
             throw new MyOwnException("Don't want to print j as zero");
         }  
@@ -22,15 +22,10 @@ class Demo1
         {
             System.out.println(e);
         } 
-        catch(ArithmeticException e)
+        catch(Exception e)
         {
              System.out.println(e);
         }
-        finally
-        {
-            System.out.println("Finished");
-        }
-        System.out.print(j); 
-
+        System.out.println(j);
     }
 }

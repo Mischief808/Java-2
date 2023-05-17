591f5678b046b765140112261c68f410c0264082
SAM-LCU <articulatemischief@gmail.com>
2023-05-17 20:08:07 +0530

    Ok

---
+++ MultiThreading/Methods/Parent.java
@@ -0,0 +1,23 @@
+class Parent extends Child
+{
+    public static void main(String a[])
+    {
+       Child c = new Child();
+       c.start();
+       c.interrupt();
+       c.interrupt();
+       for(int i=1;i<=5;i++)
+       {
+           System.out.println("Main Thread " +i);
+       }
+       try
+       {
+       c.join();
+       }
+       catch(Exception e)
+       {
+        System.out.println(e);
+       }
+       System.out.println("Thank You");
+    }
+}

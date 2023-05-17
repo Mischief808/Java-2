f371a782ff88b1b5c14be5b4630ddb44c69064ec
SAM-LCU <articulatemischief@gmail.com>
2023-05-17 20:07:44 +0530

    inclusion of prog Vehicle.java

---
+++ MultiThreading/Tata.java
@@ -0,0 +1,11 @@
+public class Tata implements Runnable
+{
+    public void run() 
+    {
+      for(int i=1;i<6;i++)
+      {
+      System.out.println("Tata vehicles are safe and reliable "+i);
+      }    
+    }
+
+}

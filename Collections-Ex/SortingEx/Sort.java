b3f91c6dbcbbb6983621214174233b458ac97855
SAM-LCU <articulatemischief@gmail.com>
2023-05-22 22:03:48 +0530

    Sorting using array class

---
+++ Collections-Ex/SortingEx/Sort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import SortPack.*;
+class Sort
+{
+    public static void main(String a[])
+    {
+        String[] bike = {"Kawasaki","BMW","Ducati","Yamaha"};
+        System.out.println("..........Before Sorting..........");
+        for(String b:bike)
+        {
+            System.out.println(b);
+        }
+        System.out.println("...........AFter Sorting..........");
+        Comparator com = new ComparatorDemo();
+        Arrays.sort(bike,com);
+        for(String b:bike)
+        {
+            System.out.println(b);
+        }
+    }
+}

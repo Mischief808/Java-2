3f3e9d108da17fc3dbd4f3f501dfde1e57472737
SAM-LCU <articulatemischief@gmail.com>
2023-05-26 11:39:35 +0530

    compare meth class

--- Collections-Ex/SortingEx/SortPack/ComparatorDemo.java
+++ Collections-Ex/SortingEx/SortPack/ComparatorDemo.java
@@ -6,17 +6,37 @@ public class ComparatorDemo implements Comparator
     {
         String s1 = (String)o1;
         String s2 = (String)o2;
-        //Descending order
-        if(s1.length() > s2.length())
-        return -1;
-        else if(s1.length() < s2.length())
-        return 1;
-        else 
-        return 0;
-        //Ascending order
+        //Descending order by length of string
         // if(s1.length() > s2.length())
         // return -1;
         // else if(s1.length() < s2.length())
+        // return 1;
+        // else 
+        // return 0;
+
+        //Ascending order by length of string
+        // if(s1.length() > s2.length())
+        // return 1;
+        // else if(s1.length() < s2.length())
+        // return -1;
+        // else 
+        // return 0;
+
+        int result = s1.compareTo(s2);
+
+        //Ascending order by alphabets
+    
+        if(result>0)
+        return 1;
+        else if(result<0)
+        return -1;
+        else 
+        return 0;
+        
+        //Descending order by alphabets
+        // if(result>0)
+        // return -1;
+        // else if(result<0)
         // return 1;
         // else 
         // return 0;

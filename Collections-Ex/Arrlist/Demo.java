fdabef42a979f50db2d3e8d7875e6a17e8cef19b
SAM-LCU <articulatemischief@gmail.com>
2023-06-09 15:41:50 +0530

    small corrections in Ex prog for ArrayLists class including some of meths

--- Collections-Ex/Arrlist/Demo.java
+++ Collections-Ex/Arrlist/Demo.java
@@ -12,9 +12,11 @@ class Demo
         ob.add(56.78);
         ob.add(false);
         System.out.println(ob);
-        System.out.println(ob.get(3));
-        System.out.println(ob.contains("Prabhu"));
-        ob.remove(4);
+        System.out.println(ob.get(3));//gets the element present at specified index
+        System.out.println(ob.contains("Prabhu"));//checks whether the element is present in ArrayList or not
+        ob.remove(4);//removes element at particular index
         System.out.println(ob);
+        ob.set(2,68);//sets the mentioned element at mentioned index
+        
     }
 }

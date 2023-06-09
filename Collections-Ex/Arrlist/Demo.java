eb0618375ab5dcb8d343ee5a4db2ae918a2e104f
SAM-LCU <articulatemischief@gmail.com>
2023-06-09 15:32:01 +0530

    Ex prog for ArrayLists class

---
+++ Collections-Ex/Arrlist/Demo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String[] a)
+    {
+        ArrayList ob = new ArrayList();
+        ob.add(26);
+        ob.add("Ramesh");
+        ob.add('z');
+        ob.add(48);
+        ob.add("Zabuilla");
+        ob.add(56.78);
+        ob.add(false);
+        System.out.println(ob);
+    }
+}
